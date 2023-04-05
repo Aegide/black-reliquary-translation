b914c3fbdf4054e60cd6b6a401a3b2b5e96b33c4
Aegide <geoffroygressier1@yahoo.fr>
2023-04-05 23:46:26 +0200

    Update build script

--- bin/localization.sh
+++ bin/localization.sh
@@ -1,2 +1,2 @@
 cp build/*.xml /c/'Program Files (x86)'/Steam/steamapps/common/'Black Reliquary'/localization
-(cd /c/'Program Files (x86)'/Steam/steamapps/common/'Black Reliquary'/_windows && ./localization.exe)
+(cd /c/'Program Files (x86)'/Steam/steamapps/common/'Black Reliquary'/_windows && ./localization.exe >nothing)
