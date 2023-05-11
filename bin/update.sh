6a66383f071288a3d2152082fce0fc9090e4bebe
Aegide <geoffroygressier1@yahoo.fr>
2023-05-11 23:34:13 +0200

    Update update script

--- bin/update.sh
+++ bin/update.sh
@@ -1,2 +1,5 @@
 rm ./black_reliquary/*.xml
 cp /c/'Program Files (x86)'/Steam/steamapps/common/'Black Reliquary'/localization/*.xml ./black_reliquary/
+rm ./black_reliquary/PSN.string_table.xml
+rm ./black_reliquary/switch.string_table.xml
+rm ./black_reliquary/xb1.string_table.xml
