28594c4dc3507b92e94d2e6d13b86f7b8fb35332
Aegide <geoffroygressier1@yahoo.fr>
2023-06-03 11:24:58 +0200

    update localization script

--- bin/localization.sh
+++ bin/localization.sh
@@ -1,5 +1,17 @@
 # clear;sh bin/localization.sh
+
 python bin/main.py
+
 cp build/*.xml /c/'Program Files (x86)'/Steam/steamapps/common/'Black Reliquary'/localization
+
 (cd /c/'Program Files (x86)'/Steam/steamapps/common/'Black Reliquary'/_windows && ./localization.exe) > build.txt
-cat build.txt
+
+echo " "
+echo "---------------------------------"
+echo " "
+
+cat build.txt | grep -v "SUCCESS!"
+
+cp /c/'Program Files (x86)'/Steam/steamapps/common/'Black Reliquary'/localization/missing_strings.csv missing_strings.csv
+
+cat missing_strings.csv | grep -v ",PSN," | grep -v ",switch," | grep -v ",xb1,"
