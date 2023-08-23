fa967cb09ea8b9fc2314e8f50de0a0bb3e752731
Aegide <geoffroygressier1@yahoo.fr>
2023-08-24 00:54:06 +0200

    Update script

--- bin/update.sh
+++ bin/update.sh
@@ -1,15 +1,28 @@
-rm ./black_reliquary/*.xml 2> /dev/null
-cp /c/'Program Files (x86)'/Steam/steamapps/common/'Black Reliquary'/localization/*.xml ./black_reliquary/
-rm ./black_reliquary/PSN.string_table.xml
-rm ./black_reliquary/switch.string_table.xml
-rm ./black_reliquary/xb1.string_table.xml
+# rm ./black_reliquary/*.xml 2> /dev/null
+# cp /c/'Program Files (x86)'/Steam/steamapps/common/'Black Reliquary'/localization/*.xml ./black_reliquary/
+# rm ./black_reliquary/PSN.string_table.xml
+# rm ./black_reliquary/switch.string_table.xml
+# rm ./black_reliquary/xb1.string_table.xml
 
-rm ./compact/*.xml 2> /dev/null
+# rm ./compact/*.xml 2> /dev/null
 cp ./black_reliquary/*.xml ./compact/
 
-sed -i 's/<!--.*-->//g' ./compact/*.xml
+# Removeq comments
+sed -i 's@<!--.*-->@@g' ./compact/*.xml
+
+# Removeq root tags
 sed -i 's@<root>@@g' ./compact/*.xml
 sed -i 's@</root>@@g' ./compact/*.xml
+
+# Removes language tags
 sed -i 's@<language id="english">@@g' ./compact/*.xml
 sed -i 's@</language>@@g' ./compact/*.xml
-sed -i 's/<?xml version="1.0" encoding="UTF-8"?>//g' ./compact/*.xml
+
+# Removes xml tag
+sed -i 's@<?xml version="1.0" encoding="UTF-8"?>@@g' ./compact/*.xml
+
+# Removes characters before "<entry>"
+sed -i 's@^.*<entry@<entry@g' ./compact/*.xml
+
+# Removes useless line returns
+sed -i -z 's@entry>[\n\t]*<entry@entry>\n<entry@g' ./compact/*.xml
