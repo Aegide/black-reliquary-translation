3564b1e5394cb79145100f7bdc6bf30cb2265cdd
Aegide <geoffroygressier1@yahoo.fr>
2023-08-24 00:00:38 +0200

    Update script

--- bin/update.sh
+++ bin/update.sh
@@ -3,3 +3,13 @@ cp /c/'Program Files (x86)'/Steam/steamapps/common/'Black Reliquary'/localizatio
 rm ./black_reliquary/PSN.string_table.xml
 rm ./black_reliquary/switch.string_table.xml
 rm ./black_reliquary/xb1.string_table.xml
+
+rm ./compact/*.xml 2> /dev/null
+cp ./black_reliquary/*.xml ./compact/
+
+sed -i 's/<!--.*-->//g' ./compact/*.xml
+sed -i 's@<root>@@g' ./compact/*.xml
+sed -i 's@</root>@@g' ./compact/*.xml
+sed -i 's@<language id="english">@@g' ./compact/*.xml
+sed -i 's@</language>@@g' ./compact/*.xml
+sed -i 's/<?xml version="1.0" encoding="UTF-8"?>//g' ./compact/*.xml
