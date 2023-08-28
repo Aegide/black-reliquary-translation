8d2ff4cf60ccd4ca415a3c3e69452f906c1ae425
Aegide <geoffroygressier1@yahoo.fr>
2023-08-28 19:54:08 +0200

    Update script

--- bin/update.sh
+++ bin/update.sh
@@ -1,16 +1,18 @@
-# rm ./black_reliquary/*.xml 2> /dev/null
-# cp /c/'Program Files (x86)'/Steam/steamapps/common/'Black Reliquary'/localization/*.xml ./black_reliquary/
-# rm ./black_reliquary/PSN.string_table.xml
-# rm ./black_reliquary/switch.string_table.xml
-# rm ./black_reliquary/xb1.string_table.xml
+# Gets black_reliquary files
+rm ./black_reliquary/*.xml 2> /dev/null
+cp /c/'Program Files (x86)'/Steam/steamapps/common/'Black Reliquary'/localization/*.xml ./black_reliquary/
+rm ./black_reliquary/PSN.string_table.xml
+rm ./black_reliquary/switch.string_table.xml
+rm ./black_reliquary/xb1.string_table.xml
 
-# rm ./compact/*.xml 2> /dev/null
+# Gets compact files
+rm ./compact/*.xml 2> /dev/null
 cp ./black_reliquary/*.xml ./compact/
 
-# Removeq comments
+# Removes comments
 sed -i 's@<!--.*-->@@g' ./compact/*.xml
 
-# Removeq root tags
+# Removes root tags
 sed -i 's@<root>@@g' ./compact/*.xml
 sed -i 's@</root>@@g' ./compact/*.xml
 
@@ -18,7 +20,7 @@ sed -i 's@</root>@@g' ./compact/*.xml
 sed -i 's@<language id="english">@@g' ./compact/*.xml
 sed -i 's@</language>@@g' ./compact/*.xml
 
-# Removes xml tag
+# Removes xml tags
 sed -i 's@<?xml version="1.0" encoding="UTF-8"?>@@g' ./compact/*.xml
 
 # Removes characters before "<entry>"
