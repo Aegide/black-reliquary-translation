06444dd68173e9cb39235fc837e816c93d5d0312
Aegide <geoffroygressier1@yahoo.fr>
2023-09-01 21:03:54 +0200

    Update localization script

--- bin/localization.sh
+++ bin/localization.sh
@@ -4,11 +4,12 @@ echo " "
 echo ">> CLEANING"
 echo " "
 
-(cd "/c/Program Files (x86)/Steam/steamapps/common/Black Reliquary/localization/";ls -l *.loc2 | cut -d " " -f 9;rm *.loc2) 2> /dev/null
+(cd "/c/Program Files (x86)/Steam/steamapps/common/Black Reliquary/localization/";ls -l *.loc2 | cut -d " " -f 10;rm *.loc2) 2> /dev/null
 rm review/*.xml
 rm build/*.xml
 echo "" > "/c/Program Files (x86)/Steam/steamapps/common/Black Reliquary/localization/missing_strings.csv"
 
+echo " "
 echo " "
 echo ">> LOCALIZING"
 echo " "
@@ -23,7 +24,7 @@ echo " "
 cp build/*.xml /c/'Program Files (x86)'/Steam/steamapps/common/'Black Reliquary'/localization
 (cd "/c/Program Files (x86)/Steam/steamapps/common/Black Reliquary/_windows" && ./localization.exe) > build.txt
 cat build.txt | grep -v "SUCCESS!" | grep -v '\-\-' | grep -v "Building" | grep -v "  "
-(cd "/c/Program Files (x86)/Steam/steamapps/common/Black Reliquary/localization/"; ls -l *.loc2 | cut -d " " -f 9)
+(cd "/c/Program Files (x86)/Steam/steamapps/common/Black Reliquary/localization/"; ls -l *.loc2 | cut -d " " -f 10)
 cp /c/'Program Files (x86)'/Steam/steamapps/common/'Black Reliquary'/localization/missing_strings.csv missing_strings.csv 2> /dev/null
 
 echo " "
